7bbc9e75a45830539009f29325b32c183d422766
Kerrovitarr82 <kokosk29@gmail.com>
2022-03-17 00:16:45 +0300

    Исправления в соответствии с изменениями

--- task-3/fourth/Main.java
+++ task-3/fourth/Main.java
@@ -5,45 +5,45 @@ public class Main {
         Admin admin = new Admin();
         Guest guest1 = new Guest("Вова");
         Guest guest2 = new Guest("Федя");
-        System.out.println(admin.getRooms().toString());
-        System.out.println(admin.getServices().toString());
+        System.out.println("\n" + admin.getRooms().toString() + "\n");
+        System.out.println(admin.getServices().toString() + "\n");
 
-        admin.addToRoom(1 - 1, guest1);                        // тут и далее -1 нужен для корректной записи в ArrayList
-        admin.addToRoom(4 - 1, guest2);                        // (в будущем скорее всего придется заменить первую цифру на переменную типа int,
-        System.out.println("Заселили двух гостей в 1 и 4 номера");      // которую будет с клавиатуры вводить юзер)
-        System.out.println(admin.getRooms().get(1 - 1).toString());
-        System.out.println(admin.getRooms().get(4 - 1).toString() + "\n");
+        admin.addToRoom(1, guest1);
+        admin.addToRoom(4, guest2);
+        System.out.println("Заселили двух гостей в 1 и 4 номера");
+        System.out.println(admin.getRoom(1).toString());
+        System.out.println(admin.getRoom(4).toString() + "\n");
 
-        admin.deleteFromRoom(1 - 1);
+        admin.deleteFromRoom(1);
         System.out.println("Выселили гостя из 1 номера");
-        System.out.println(admin.getRooms().get(1 - 1).toString() + "\n");
+        System.out.println(admin.getRoom(1).toString() + "\n");
 
-        admin.underRepair(3 - 1);
+        admin.changeStatus(3, Status.UNDER_REPAIR);
         System.out.println("Отправили 3 номер на ремонт");
-        System.out.println(admin.getRooms().get(3 - 1).toString() + "\n");
+        System.out.println(admin.getRoom(3).toString() + "\n");
 
-        admin.serviced(4 - 1);
+        admin.changeStatus(4, Status.SERVICED);
         System.out.println("Отправили 4 номер на обслуживание");
-        System.out.println(admin.getRooms().get(4 - 1).toString() + "\n");
+        System.out.println(admin.getRoom(4).toString() + "\n");
 
-        admin.changePriceToRoom(5 - 1, 7000);
+        admin.changePriceToRoom(5, 7000);
         System.out.println("Изменили цену на 5 номер");
-        System.out.println(admin.getRooms().get(5 - 1).toString() + "\n");
+        System.out.println(admin.getRoom(5).toString() + "\n");
 
-        admin.changePriceToService("Завтрак", 500);
+        admin.changePriceToService(1, 500);
         System.out.println("Изменили цену на услугу <<Завтрак>>");
         for (Service service : admin.getServices()) {
-            if (service.getName() == "Завтрак") {
-                System.out.println(service.toString());
+            if (service.getId() == 1) {
+                System.out.println(service + "\n");
                 break;
             }
         }
 
-        admin.addRoom(10000);
+        admin.addRoom(10000, 6);
         System.out.println("Добавили номер 6 с ценой 10000");
-        System.out.println(admin.getRooms().get(6 - 1).toString() + "\n");
+        System.out.println(admin.getRoom(6).toString() + "\n");
 
-        admin.addService("Обед", 500);
+        admin.addService("Обед", 500, 2);
         System.out.println("Добавили новую услугу <<Обед>> с ценой 500");
         System.out.println(admin.getServices().toString());
 
