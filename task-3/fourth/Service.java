6ccbd173698314e512b5bf025d2a5bc198bba3e1
Kerrovitarr82 <kokosk29@gmail.com>
2022-03-17 00:14:52 +0300

    Добавлен id

--- task-3/fourth/Service.java
+++ task-3/fourth/Service.java
@@ -3,10 +3,12 @@ package fourth;
 public class Service {
     private String name;
     private int price;
+    private int id;
 
-    public Service(String name, int price) {
+    public Service(String name, int price, int id) {
         this.name = name;
         this.price = price;
+        this.id = id;
     }
 
     public String getName() {
@@ -25,11 +27,20 @@ public class Service {
         this.price = price;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "Service{" +
                 "name='" + name + '\'' +
                 ", price=" + price +
+                ", id=" + id +
                 '}';
     }
 }
